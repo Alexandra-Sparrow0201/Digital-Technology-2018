75cb8b0a7ef9b4b1e81a4b6449b9e1fdf76703ba
Alexandra-Sparrow0201 <35312741+Alexandra-Sparrow0201@users.noreply.github.com>
2018-04-04 09:22:35 +1200

    Start Up
    Contains desciption and purpose.

---
+++ imc.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+# Name:       imc.py
+# Purpose: imc(Image Metadata Collector).
+#The program will collect image metadata.
+#Versioning and progress will be managaed through GitHub
+#
+# Author:      halla, WEGC
+#
+# Created:     04/04/2018
+
