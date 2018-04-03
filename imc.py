8a3e4ec15e0f65e3eb4d241d815b2eb37459c627
Alexandra-Sparrow0201 <35312741+Alexandra-Sparrow0201@users.noreply.github.com>
2018-04-04 10:04:44 +1200

    Class setup
    Image Metadata class created, with get functions. Plan for variables.

--- imc.py
+++ imc.py
@@ -8,3 +8,36 @@
 #
 # Created:     04/04/2018
 
+class ImageMD:
+    #Image Metadata
+    def __init__(self, imageID, fileName, fileExtension, imageName, owner, licence, photoCategory):
+        self.imageID= imageID
+        self.fileName= fileName
+        self.fileExten= fileExtension
+        self.name= imageName
+        self.owner= owner
+        self.licence= licence
+        self.photoC= photoCategory
+
+    def get_imageID(self):
+        return self.imageID
+
+    def get_fileName(self):
+        return self.fileName
+
+    def get_fileExten(self):
+        return self.fileExten
+
+    def get_name(self):
+        return self.name
+
+    def get_owner(self):
+        return self.owner
+
+    def get_licence(self):
+        return self.licence
+
+    def get_photoC(self):
+        return self.photoT
+
+
