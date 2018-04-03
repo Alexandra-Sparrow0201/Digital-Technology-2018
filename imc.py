8751211aa2cef25d4b4378d28455c9e389ddff93
Alexandra-Sparrow0201 <35312741+Alexandra-Sparrow0201@users.noreply.github.com>
2018-04-04 10:16:44 +1200

    Start
    Create Class GUI.
    Imported tkinder library
    window created, does not open.

--- imc.py
+++ imc.py
@@ -8,6 +8,12 @@
 #
 # Created:     04/04/2018
 
+#import GUI library
+from tkinter import *
+
+#for Python V3 you must explicitely load the messagebox
+import tkinter.messagebox
+
 class ImageMD:
     #Image Metadata
     def __init__(self, imageID, fileName, fileExtension, imageName, owner, licence, photoCategory):
@@ -41,3 +47,25 @@ class ImageMD:
         return self.photoT
 
 
+class GUI:
+    #default colour variables makes changing colours easier
+    bkcolour= '#c0deed'
+    maintxtcolour= 'black'
+    mainfont = ('Times', '24')
+
+    def __init__(self):
+
+        window = Tk()
+        window.title("Data Entry for Image Metadata")
+        window.minsize(width = 400, height= 600)
+
+        heading_label = Lable(window, bg = bkcolour, fg = maintxtcolour, text= "TITLE HERE", font= mainfont)
+        header_label.pack()
+
+        #creating label and field variable in GUI for each entry field
+        fileName_label = Label(window, text='Please enter the filename:')
+        fileName_label.pack(anchor="c") #.pack() places the component in the window
+        self.fileName_field = Entry(window)
+        self.fileName_field.pack(anchor="c")
+
+        #code for dropdown menu
