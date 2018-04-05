170b62c7569fe3777b4f423fab088afab9ce20ae
Alexandra-Sparrow0201 <35312741+Alexandra-Sparrow0201@users.noreply.github.com>
2018-04-06 10:33:19 +1200

    writetocsv
    Function write to CSV completed.

--- imc.py
+++ imc.py
@@ -38,6 +38,32 @@ class ImageMD:
         return self.licence
 
     def get_photoC(self):
-        return self.photoT
+        return self.photoC
 
+    def full_file_name(self): #To show filenames with extensions
+        together= fileName + fileExten
+        return together
 
+class GUI:
+
+    def writetocsv(self):
+        #This what happens upon clicking the "Write to CSV" button
+        import csv
+        file_name = 'test1.csv' #I prefer csv files are automativally opened by excel rather than notepad
+
+        if self.ready_to_write: #Connects to function that checks data has been validated
+            ofile = open(file_name, 'w') #Open file with overwriting permissions.
+            writer = csv.writer(ofile, delimiter =",", lineterminator = "\n")
+            for record in self.recordlist:
+                print(record.full_file_name())
+                writer.writerow([record.get_imageID(), record.get_fileName, record.get_fileExtension(), record.get_imageName(), record.get_owner(), record.get_licence(), record.get_photoC()])
+
+            ofile.close()#To Explicitly close file
+
+        else:
+            tkinter.messagebox.showwaring("Error!", "You need to validate your data first!")
+
+        self.ready_to_write= False
+        tkinter.messagebox.showinfo('Notice',file_name+' File Generated Sucessfully')
+
+GUI()
